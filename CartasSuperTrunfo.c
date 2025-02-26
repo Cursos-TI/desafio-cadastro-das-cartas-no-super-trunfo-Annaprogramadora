fc354aad9d4e003f125e307ced3f6a654301718e
Annaprogramadora <annaflaviaoliveira07@gmail.com>
2025-02-26 15:52:57 +0000

    Fiz as duas cartas e rodou perfeitamente

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,49 +11,62 @@ int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
 
-        //Carta 1
-    char estado1[50] = "Mato Grosso";    //Nome do estado
-    char código1[50] = "A01";   //Código da cidade
-    char nome_cidade1[50] = "Cuiabá" ;
-    float população1 = 650.877;  //População da cidade
-    float área_km²1 = 3.538;    //Área em km² da cidade
-    float PIB1 = 47700.88;      //PIB da cidade
-    int número_pontos_turísticos1 = 15;   //Número de pontos turísticos da cidade
+       //Carta 1
+       char nome1[20] = "Mato Grosso";    //nome do estado
+       char codigo1[20] = "A01";          //código da cidade
+       char nome_cidade1[20] = "Cuiabá";  //nome da cidade
+       int populacao1 = 650877;      // Corrigido para não ter ponto
+       float area_km1 = 3538.0;      // área em km² da cidade
+       float PIB1 = 47700.88;        //PIB da cidade
+       int numero_pontos_turisticos1 = 50;  //número de pontos turísticos da cidade
+   
+    // Cadastro das Cartas: 
+     printf("Carta 01\n");
 
-    // Cadastro das Cartas:
-    printf("Insira a inicial do estado: ");
-    scanf("%c", estado1);
-    printf("A inicial é: %c\n", estado1);
+     printf("Estado: %s\n", nome1);
 
-    printf("Insira o código da cidade:" );
-    scanf("%s", código1);
-    printf("O código da cidade é: %s", código1);
+     printf("Código: %s\n", codigo1);
 
-    printf("Insira o nome da cidade: ");
-    scanf("%s", nome_cidade1);
-    printf("O nome da cidade é: %s", nome_cidade1);
+     printf("Nome da cidade: %s\n", nome_cidade1);
 
-    printf("Insira a população da cidade: ");
-    scanf("%.3f", população1);
-    printf("A populaçaão da cidade é: %.3f", população1);
+     printf("População: %d\n", populacao1); // Corrigido para %d
+
+     printf("Área em km²: %.2f\n", area_km1);
+
+     printf("PIB: %.2f\n", PIB1);
+
+     printf("Número de pontos turísticos: %d\n", numero_pontos_turisticos1);
+     printf("\n");       //para pular linha entre a carta 1 e carta 2
     
-    printf("Insira a área da cidade em km²: ");
-    scanf("%.3f", área_km²1);
-    printf("A área da cidade em km² é: %.3f", área_km²1);
+       //Carta 2
+       char nome2[20] = "Mato Grosso";    //nome do estado
+       char codigo2[20] = "B02";          //código da cidade
+       char nome_cidade2[20] = "Cáceres";  //nome da cidade
+       int populacao2 = 91626;           // Corrigido para não ter ponto
+       float area_km2 = 24499.922;      // área em km² da cidade
+       float PIB2 = 438136155.50;        //PIB da cidade
+       int numero_pontos_turisticos2 = 21;  //número de pontos turísticos da cidade
 
-    printf("Insira o PIB da cidade: ");
-    scanf("%.2f", PIB1);
-    printf("O PIB da cidade é: %.2f", PIB1);
-    
-    printf("insira o número de pontos turísticos existentes na cidade: ");
-    scanf("%d", número_pontos_turísticos1);
-    printf("O número de pontos turísticos é: %d", número_pontos_turísticos1);
+       //Cadrasto das cartas
+       printf("Carta 02\n");
 
+       printf("Estado: %s\n", nome2);
+   
+       printf("Código: %s\n", codigo2);
+   
+       printf("Nome da cidade: %s\n", nome_cidade2);
+   
+       printf("População: %d\n", populacao2); // Corrigido para %d
+   
+       printf("Área em km²: %.2f\n", area_km2);
+   
+       printf("PIB: %.2f\n", PIB2);
+   
+       printf("Número de pontos turísticos: %d\n", numero_pontos_turisticos2);
+   
 
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
